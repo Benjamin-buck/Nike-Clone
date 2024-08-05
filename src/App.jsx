335090d46107414801c11a35aefb813f85210efe
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-08-05 14:00:13 -0400

    Completes NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,66 @@
-export default function App() {
+import CustomerReviews from './sections/CustomerReviews';
+import Footer from './sections/Footer';
+import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import Subscribe from './sections/Subscribe';
+import SuperQuality from './sections/SuperQuality';
+import Nav from './components/Nav';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      <Nav />
+
+      {/* HERO SECTION */}
+      <section className="xl:padding-1 wide:padding-r padding:b">
+        <Hero />
+      </section>
+
+
+      {/* POPULAR PRODUCTS SECTION */}
+      <section className="padding">
+        <PopularProducts />
+      </section>
+
+
+      {/* SUPER QUALITY SECTION */}
+      <section className="padding">
+        <SuperQuality />
+      </section>
+
+
+      {/* SERVICES SECTION */}
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+
+
+      {/* SPECIAL OFFER SECTION */}
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+
+      
+      {/* CUSTOMER REVIEWS SECTION */}
+      <section className="padding bg-pale-blue">
+        <CustomerReviews/>
+      </section>
+
+
+      {/* SUBSCRIBE SECTION */}
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+
+
+      {/* FOOTER SECTION */}
+      <section className="padding-x padding-t bg-black pb-8">
+        <Footer></Footer>
+      </section>
+    </main>
   )
 }
+
+export default App
